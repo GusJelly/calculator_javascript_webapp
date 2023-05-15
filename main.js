16f7157e70d2636653140cfb3864270d9d277d87
marmelo <marmelo@noreply.com>
2023-05-15 01:42:23 +0100

    fixed files

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello world");
-
-// TODO:
-// Add eventhandler to capture clicks on the buttons;
-//
-// make those clicks reflect on the screen item;
-// parse the screen's inner html to be able to make the calculations;
-
